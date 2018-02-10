ce462af57d80b6cb480e335b5baf537bfd796923
Muhammad Rizwan <mrizwanghuman@yahoo.com>
2018-02-10 01:50:40 -0500

    No internet error handling

--- app/src/main/java/com/pro/rizwan/weathertestapp/splashscreen/SplashScreen.java
+++ app/src/main/java/com/pro/rizwan/weathertestapp/splashscreen/SplashScreen.java
@@ -1,20 +1,47 @@
 package com.pro.rizwan.weathertestapp.splashscreen;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.pro.rizwan.weathertestapp.R;
 import com.pro.rizwan.weathertestapp.homeactivity.HomeActivity;
 
 public class SplashScreen extends AppCompatActivity {
 Intent homeScreenIntent;
+    private boolean connected;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        startHomeActivity();
+        connected = false;
+        ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        if(connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
+                connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
+            //we are connected to a network
+            connected = true;
+        }
+        else {
+            connected = false;
+        }
+        if (connected) {
+            startHomeActivity();
+        }else {
+            Toast.makeText(this, "Please check your internet connection", Toast.LENGTH_SHORT).show();
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    finish();
+                }
+            }, 1000);
+
+        }
     }
     public   void startHomeActivity(){
          homeScreenIntent                = new Intent(this, HomeActivity.class);
@@ -28,4 +55,5 @@ Intent homeScreenIntent;
         }, 2000);
 
     }
+
 }
